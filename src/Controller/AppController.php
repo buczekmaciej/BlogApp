690cc593c258275647d72b0624bf0b428f6f363c
Maciej Czerniawski <Wchodz3WTo@gmail.com>
2019-06-25 11:05:05 +0200

    Fixes

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -4,14 +4,20 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class AppController extends AbstractController
 {
     /**
      * @Route("/", name="appHomepage")
      */
-    public function homepage()
+    public function homepage(SessionInterface $session)
     {
+        $logged=$session->get('user');
+        if(!$logged)
+        {
+            return $this->redirectToRoute('userLogin', []);
+        }
         return $this->render('app/index.html.twig', []);
     }
 }
