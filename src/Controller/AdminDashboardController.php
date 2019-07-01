16a7999890c96f9049a38cb859a0627f1f01802c
Maciej Czerniawski <Wchodz3WTo@gmail.com>
2019-07-01 17:13:25 +0200

    Post show layout fix

--- src/Controller/AdminDashboardController.php
+++ src/Controller/AdminDashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 
 class AdminDashboardController extends AbstractController
 {
@@ -17,6 +18,16 @@ class AdminDashboardController extends AbstractController
         return $this->render('admin_dashboard/dash.html.twig', [
             'user'=>$user
         ]);
+    }
+
+    /**
+     * @Route("/admin/dashboard/logout", name="dashLogout")
+     */
+    public function dashLogout()
+    {
+        $this->get('security.token_storage')->setToken(null);
+
+        return $this->redirectToRoute('appHomepage', []);
     }
 
 }
