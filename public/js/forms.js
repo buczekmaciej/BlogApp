28c34f2fa30fc4bcdc06415fac2461303dc84eb9
Maciek <Wchodz3WTo@gmail.com>
2020-08-11 19:14:59 +0200

    Login - Fixed inputs labels/Added js to manage disabled attrib for button

--- public/js/forms.js
+++ public/js/forms.js
@@ -1,10 +1,24 @@
-let password = document.getElementsByClassName("pass")[0];
 let show = document.getElementsByClassName("showBtn")[0];
 
 show.onclick = () => showPassword();
 
 function showPassword() {
+  let password = document.getElementsByClassName("pass")[0];
+
   password.getAttribute("type") == "password"
     ? password.setAttribute("type", "text")
     : password.setAttribute("type", "password");
+}
+
+if (window.location.pathname == "/u/login") {
+  let username = document.getElementById("username");
+  let password = document.getElementById("password");
+
+  username.onkeyup = () => checkLogin(username.value, password.value);
+  password.onkeyup = () => checkLogin(username.value, password.value);
+}
+
+function checkLogin(usr, pass) {
+  if (usr && pass) document.getElementById("submit-login").disabled = false;
+  else document.getElementById("submit-login").disabled = true;
 }
