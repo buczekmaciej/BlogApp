e9c6c45b5aba9fd78e98f236643b1318960be352
Maciej <Wchodz3WTo@gmail.com>
2021-04-07 14:46:00 +0200

    Login/register

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Form\RegisterType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -21,6 +26,45 @@ class SecurityController extends AbstractController
         return $this->render('security/login.html.twig', [
             'last_username' => $authenticationUtils->getLastUsername(),
             'error' => $authenticationUtils->getLastAuthenticationError(),
+        ]);
+    }
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register(?string $error = null, Request $request, EntityManagerInterface $entityManager, UserPasswordEncoderInterface $encoder, UserRepository $userRepository): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home');
+        }
+
+        $registerForm = $this->createForm(RegisterType::class);
+        $registerForm->handleRequest($request);
+
+        if ($registerForm->isSubmitted() && $registerForm->isValid()) {
+            $user = $registerForm->getData();
+
+            if (!$userRepository->registerValidateData($user->getUsername(), $user->getEmail())) {
+                $user->setPassword($encoder->encodePassword($user, $user->getPassword()));
+                $user->setRoles(['ROLE_USER']);
+
+                $preferences = new \App\Entity\Preference;
+                $user->setPreferences($preferences);
+
+                $entityManager->persist($preferences);
+                $entityManager->persist($user);
+
+                $entityManager->flush();
+
+                return $this->redirectToRoute('login');
+            } else {
+                $error = "Username or e-mail is already taken";
+            }
+        }
+
+        return $this->render('security/register.html.twig', [
+            'form' => $registerForm->createView(),
+            'error' => $error,
         ]);
     }
 
---
+++ src/Form/RegisterType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegisterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class, [
+                'label' => 'Username',
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Password',
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'E-mail',
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Create account',
+                'attr' => [
+                    'class' => 'form-btn',
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'register';
+    }
+}
